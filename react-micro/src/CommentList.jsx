4d16edc62c669b5dedda972545b25d3fe1e49602
Mohsen rajabpour <Mh3n.rj@Gmail.com>
2024-03-04 16:15:58 +0330

    #

--- react-micro/src/CommentList.jsx
+++ react-micro/src/CommentList.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const CommentList = ({ postId }) => {
   const [comments, setComments] = useState([]);
-
+// console.log(postId);
   const fetchData = async () => {
     const res = await axios.get(
       `http://localhost:4001/posts/${postId}/comments`
@@ -17,7 +17,19 @@ const CommentList = ({ postId }) => {
   }, []);
 
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
